522d4a7d6615689c842257f995d727449d26434a
LauJosefsen <ljosef18@student.aau.dk>
2020-10-06 13:04:09 +0200

    Fixed minor stuff

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -13,7 +13,7 @@ config.read('config.ini')
 
 def run_crawler(arg_object):
     """ Runs the crawler for all specified file formats and call their respected modules
-    :param arg_object:
+    :param arg_object: object that stores the program arguments
     """
     if arg_object.clearcache or not path.exists(config['structure']['cache_file']):
         # recalculate folders.json
@@ -36,15 +36,16 @@ def run_crawler(arg_object):
         arg_object.fromDate = folders[0]
 
     # checks if the to date is after the from date
-    int_from_date = string_to_int(arg_object.fromDate)
-    int_to_date = string_to_int(arg_object.toDate)
+    pprint(arg_object)
+    int_from_date = date_to_int(arg_object.fromDate)
+    int_to_date = date_to_int(arg_object.toDate)
     if int_from_date > int_to_date:
-        print("The to date is before the from date.")
+        raise Exception("The to-date is before the from-date.")
 
     # Filter folders by dates
-    toDate = string_to_int(arg_object.toDate)
-    fromDate = string_to_int(arg_object.fromDate)
-    folders = [folder for folder in folders if fromDate < string_to_int(folder) < toDate]
+    toDate = date_to_int(arg_object.toDate)
+    fromDate = date_to_int(arg_object.fromDate)
+    folders = [folder for folder in folders if fromDate < date_to_int(folder) < toDate]
 
     # Loops through all the folders in the path and their respective files.
     for folder in folders:
@@ -53,7 +54,7 @@ def run_crawler(arg_object):
         for file in files:
             # checks if it is a .jp2 file. if true, the ocr is called
             if ".jp2" in file:
-                #todo call module for tesseract
+                # todo call module for tesseract
                 print("Tesseract not implemented yet.")
             # checks if it is a .xml file. if true, the parser for .nitf parser is called
             if ".xml" in file:
@@ -61,9 +62,9 @@ def run_crawler(arg_object):
         output(output_this_folder, f"output.json")
 
 
-def string_to_int(a):
+def date_to_int(a):
     """ Simply converts string to int
-    :param a:
+    :param a: object that has year, month, and date properties
     :return:
     """
     return int(str(a['year']) + str(a['month']).zfill(2) + str(a['date']).zfill(2))
@@ -80,27 +81,31 @@ def find_folders_recursively(directory):
 
     found_folders = []
 
-    pattern = re.compile(config['structure']['final-folder-regex'])
+    pattern_strings = config['structure']['final-folder-regex'].split(",")
+    regexs = []
+    for pattern_string in pattern_strings:
+        print(pattern_string)
+        regexs.append(re.compile(pattern_string))
     for cdir in dirs:
-        if re.match(pattern, cdir):
-            # Add to list to return
-            found_folders.append(
-                {
-                    'path': directory + "/" + cdir,
-                    'year': int(cdir[0:4]),
-                    'month': int(cdir[5:7]),
-                    'date': int(cdir[8:10])
-                }
-            )
-        else:
-            # extend recursively
-            found_folders.extend(find_folders_recursively(directory+"/"+cdir))
+        for regex in regexs:
+            if re.match(regex, cdir):
+                # Add to list to return
+                found_folders.append(
+                    {
+                        'path': directory + "/" + cdir,
+                        'year': int(cdir[0:4]),
+                        'month': int(cdir[5:7]),
+                        'date': int(cdir[8:10])
+                    }
+                )
+            else:
+                # extend recursively
+                found_folders.extend(find_folders_recursively(directory+"/"+cdir))
     return found_folders
 
 
 def output(folders, file_name):
-    """
-    Dumps all files from a folder
+    """ Dumps all files from a folder
     :param folders:
     :param file_name:
     """
@@ -111,8 +116,8 @@ def output(folders, file_name):
 def load(directory):
     """Loads a json file and returns its data
 
-    :param directory:
-    :return:
+    :param directory: json file to be loaded
+    :return: data from json file
     """
     with open(directory) as json_file:
         data = json.load(json_file)
@@ -120,16 +125,19 @@ def load(directory):
 
 
 def find_relevant_files_in_directory(directory):
-    """
+    """Finds all files in a directory and checks them again the file type
+    white and blacklist.
 
-    :param directory:
-    :return:
+    :param directory: path to directory
+    :return: all found files that isn't in the blacklist/whitelist
     """
     found_files = []
+    #Fetch
     files = find_all_files_recursively(directory)
     pprint(files)
     blacklist = config['structure']['blacklist'].split(",")
     whitelist = config['structure']['whitelist'].split(",")
+    # checks whether the file should be appended, by using config white- and blacklist
     for file in files:
         if is_file_in_list(file, blacklist):
             continue
@@ -147,6 +155,11 @@ def find_relevant_files_in_directory(directory):
 
 
 def find_all_files_recursively(directory):
+    """Recursivly finds all files in a directory
+
+    :param directory:
+    :return:
+    """
     print(f"Searching directory {directory} for files..")
     walk = next(os.walk(directory))
     files = walk[2]
@@ -158,6 +171,11 @@ def find_all_files_recursively(directory):
 
 
 def is_file_valid_nitf(xml_path):
+    """Checks if the XML file given has the nitf:nitf tag
+
+    :param xml_path: path to the XML file
+    :return: true or false, depending if its a nitf file or not
+    """
     xml_doc = minidom.parse(xml_path)
     if len(xml_doc.getElementsByTagName('nitf:nitf')) != 0:
         # file is nitf, lets check if it is a valid article. todo
@@ -166,6 +184,12 @@ def is_file_valid_nitf(xml_path):
 
 
 def is_file_in_list(file, checklist):
+    """Checks if the file type is in the checklist (black- or whitelist)
+
+    :param file: file to be checked
+    :param checklist: white- or blacklist from config file
+    :return:
+    """
     for listed_item in checklist:
         if listed_item in file:
             return True
