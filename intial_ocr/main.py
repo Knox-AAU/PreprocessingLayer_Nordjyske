f33259dea5852e43f71a1c7fab79186682374eea
Lena <durumguru@gmail.com>
2020-10-06 11:29:09 +0200

    Finished the tasks
    Cleaned up the code
    Can run tesseract and get condifence score
    Can save the confidence score and the words
    Can get the average confidence score
    Added a debug function
    
    Co-Authored-By: alexikik <43371371+alexikik@users.noreply.github.com>

--- intial_ocr/main.py
+++ intial_ocr/main.py
@@ -18,36 +18,24 @@ def load_file(path):
 
 
 def run_tesseract_on_image(img, language='dan'):
-    """ Runs tesseract and returns todo:beskrivelse
+    """ Runs tesseract and returns word and confidence score
     :param img: The image that tesseract runs
     :param language: The language of the image text
-    :return:todo:beskrivelse
+    :return: A matrix with the word and the corresponding confidence score
     """
     if img is None:
         return None
     arr_all_data = pytesseract.image_to_data(img, lang=language)
-    list = conf_str_to_matrix(arr_all_data)
-    print(list[5][11] + ' | ' + list[5][10])
+    matrix = conf_str_to_matrix(arr_all_data)
+    matrix = save_conf_and_text(matrix)
 
-
-"""
-    lines = arr_all_data.split('\n')
-    list = []
-
-    for item in lines:
-        subl = []
-        for num in item.split('\t'):
-            subl.append(num)
-        list.append(subl)
-
-    print(list[5][11] + ' | ' + list[5][10])
-    """
+    return matrix
 
 
 def conf_str_to_matrix(line):
     """ Converts a string to a matrix
     :param line: The string that is to be converted
-    :return: a matrix
+    :return: A matrix
     """
 
     # Splits the string into an array containing rows
@@ -56,23 +44,74 @@ def conf_str_to_matrix(line):
 
     # Splits the array into a matrix containing the columns
     for item in rows:
-        subList = []
+        sub_list = []
         for num in item.split('\t'):
-            subList.append(num)
-        columns.append(subList)
+            sub_list.append(num)
+        columns.append(sub_list)
 
-    print(columns[5][11] + ' | ' + columns[5][10])
     return columns
 
 
-# img5 = Image.open("1988.jp2", "RGB")
-img4 = Image.open("testImages/1988.jp2").convert("RGB")
-img5 = cv2.imread("testImages/test2.jpg")
+def save_conf_and_text(data_matrix):
+    """ Only saves the confidence score and the word
+    :param data_matrix: The matrix with all information from tesseract
+    :return: Matrix without redundant information
+    """
+    length = len(data_matrix) - 1
+    new_matrix = []
 
-run_tesseract_on_image(img5, 'dan')
+    for index in range(length):
+        sub_list = []
+        # index == 0 contains the header
+        if data_matrix[index][10] == "-1" or index == 0:
+            continue
+        sub_list.append(data_matrix[index][10])
+        sub_list.append(data_matrix[index][11])
+        new_matrix.append(sub_list)
+    return new_matrix
+
+
+def print_text_from_matrix(data_matrix):
+    """ Prints the text from the matrix
+    :param data_matrix: The matrix containing the words
+    """
+    length = len(data_matrix)
+
+    for index in range(length):
+        print(data_matrix[index][1], end=' ')
+    print()
+
+
+def get_average_conf_from_matrix(data_matrix):
+    """ Gets the avergae of the confidence score
+    :param data_matrix: The matrix containing the confidence score
+    :return: A float with the average confidence score
+    """
+    length = len(data_matrix)
+    num = 0
+
+    for index in range(length):
+        temp_num = int(data_matrix[index][0])
+        if 0 <= temp_num <= 100:
+            num += temp_num
+    return num / length
+
+
+def debug_prints(data_matrix):
+    print_text_from_matrix(data_matrix)
+    average_conf = get_average_conf_from_matrix(data_matrix)
+    print(average_conf)
+
+
+img4 = Image.open("testImages/1988.jp2").convert("RGB")
+img5 = Image.open("testImages/test2.jpg")
+
+# matrix = run_tesseract_on_image(img5, 'eng')
+matrix = run_tesseract_on_image(img4, 'dan')
+debug_prints(matrix)
 
 # text = pytesseract.image_to_string(img5, lang='dan')
-text = pytesseract.image_to_data(img5, lang='eng')
+# text = pytesseract.image_to_data(img5, lang='eng')
 # text = pytesseract.image_to_data(img, lang='dan')
 # print(text)
 
