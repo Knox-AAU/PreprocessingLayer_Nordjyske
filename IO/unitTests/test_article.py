ffa0778b689ee4b5c2c44be86f40a8957dfd230e
nielswps <nielswps@gmail.com>
2020-10-06 14:49:13 +0200

    WIP: testing started, article init FIXED

--- IO/unitTests/test_article.py
+++ IO/unitTests/test_article.py
@@ -1,11 +1,28 @@
-from IO.models.article import Article
+from IO.models.article import *
+from jsonschema import validate
+import requests, json
 
 
 class TestArticle:
-    def test_add_paragraph(self):
-        article = Article()
+    article: Article
 
-        assert False
+    def setup_method(self, method):
+        """ setup any state tied to the execution of the given method in a
+        class.  setup_method is invoked for every test method of a class.
+        """
+        self.article = Article()
 
-    def test_to_json(self):
-        assert False
+    def test_add_paragraph_gives_article_with_paragraph_on_paragraph(self):
+        paragraph = Paragraph()
+        self.article.add_paragraph(paragraph)
+        assert self.article.paragraphs.__len__() == 1
+
+    def test_add_paragraph_gives_article_without_paragraph_on_string(self):
+        self.article.add_paragraph("string")
+        assert self.article.paragraphs.__len__() < 1
+
+    def test_to_json_gives_json_complying_with_schema_on_call(self):
+        output = self.article.to_json()
+        response = requests.get("https://knox.libdom.net/schema/article.schema.json")
+        schema = response.json()
+        assert validate(output, schema)
