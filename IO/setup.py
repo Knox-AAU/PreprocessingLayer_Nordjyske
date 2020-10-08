beff547b432e563bce748fed2a638f74de10f2e9
nielswps <nielswps@gmail.com>
2020-10-08 08:52:56 +0200

    Python required version lowered

--- IO/setup.py
+++ IO/setup.py
@@ -19,5 +19,5 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3.0",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.6',
 )
