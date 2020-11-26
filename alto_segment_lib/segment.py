fee9896634894f36eb6b88b02b9e0e84b9caf060
Thomas Lorentzen <thomas@tlorentzen.net>
2020-11-26 08:23:01 +0100

    WIP: Segments now know its senter

--- alto_segment_lib/segment.py
+++ alto_segment_lib/segment.py
@@ -49,6 +49,9 @@ class Segment:
     def add_line(self, line):
         self.lines.append(line)
 
+    def get_center(self):
+        return (self.x2 - (self.x2 - self.x1) / 2), (self.y2 - (self.y2 - self.y1) / 2)
+
     def calculate_coordinates(self):
         if len(self.lines) == 0:
             return None
