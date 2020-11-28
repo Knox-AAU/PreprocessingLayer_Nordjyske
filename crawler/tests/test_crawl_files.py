678714e74513a85d8b06a16d0b3730b6f621d29d
LauJosefsen <ljosef18@student.aau.dk>
2020-11-28 15:53:47 +0100

    Made unittests for crawler, and added documentation to crawl.py

---
+++ crawler/tests/test_crawl_files.py
@@ -0,0 +1,82 @@
+from datetime import datetime, timezone
+from queue import Queue
+from unittest import mock
+
+from crawler.crawl import Crawler
+from crawler.file_types import FileType
+from crawler.folder import Folder
+from crawler.tests.test_crawl_folders import PseudoDirEntry
+
+
+@mock.patch("os.scandir")
+@mock.patch.object(Crawler, "is_file_valid_nitf")
+def test_crawl_files_nitf(mock_valid_nitf, mock_scandir, ):
+    mock_valid_nitf.side_effect = [True, False]
+
+    mock_scandir.side_effect = [
+        [PseudoDirEntry("SomeNitfFile.xml", "SomeNitfFile.xmlPath", False, None)],
+        [PseudoDirEntry("SomeInvalidNitfFile.xml", "SomeInvalidNitfFile.xmlPath", False, None)],
+    ]
+    q = Queue()
+    directory = ""
+
+    crawler = Crawler()
+    folder = Folder("", 1, 1, 1)
+    crawler.crawl_for_files_in_folders(folder, "")
+
+    assert len(folder.files) == 1
+    assert folder.files[0].name == "SomeNitfFile.xml"
+    assert folder.files[0].type == FileType.NITF
+
+@mock.patch("os.scandir")
+def test_crawl_files_jp2(mock_scandir, ):
+
+    mock_scandir.side_effect = [
+        [PseudoDirEntry("SomeImageFile.jp2", "SomeNitfFile.xmlPath", False, None)],
+        [PseudoDirEntry("SomeNotJp2File.txt", "SomeInvalidNitfFile.xmlPath", False, None)],
+    ]
+
+    crawler = Crawler()
+    folder = Folder("", 1, 1, 1)
+    crawler.crawl_for_files_in_folders(folder, "")
+
+    assert len(folder.files) == 1
+    assert folder.files[0].name == "SomeImageFile.jp2"
+    assert folder.files[0].type == FileType.JP2
+
+@mock.patch("os.scandir")
+def test_crawl_files_recursive(mock_scandir):
+
+    mock_scandir.side_effect = [
+        [PseudoDirEntry("folder", "/folder/", True, None)],
+        [PseudoDirEntry("SomeImageFile.jp2", "SomeNitfFile.xmlPath", False, None)],
+    ]
+
+    crawler = Crawler()
+    folder = Folder("", 1, 1, 1)
+    crawler.crawl_for_files_in_folders(folder, "")
+
+    assert len(folder.files) == 1
+    assert folder.files[0].name == "SomeImageFile.jp2"
+
+@mock.patch("os.scandir")
+def test_crawl_files_whitelist(mock_scandir):
+
+    mock_scandir.return_value = [
+        PseudoDirEntry("SomeTest.jp2", "", False, None),
+        PseudoDirEntry("SomeImage.jp2", "", False, None),
+        PseudoDirEntry("SomeFile.jp2", "", False, None),
+    ]
+
+    crawler = Crawler()
+
+    crawler.whitelist = ["File","Test"]
+    crawler.blacklist = []
+
+    folder = Folder("", 1, 1, 1)
+    crawler.crawl_for_files_in_folders(folder, "")
+
+    assert len(folder.files) == 2
+    assert next(filter(lambda file: file.name == "SomeTest.jp2", folder.files), None) is not None
+    assert next(filter(lambda file: file.name == "SomeFile.jp2", folder.files), None) is not None
+
---
+++ crawler/tests/test_crawl_folders.py
@@ -0,0 +1,67 @@
+from datetime import datetime, timezone
+from queue import Queue
+from unittest import mock
+
+from crawler.crawl import Crawler
+
+
+class PseudoDirEntry:
+    def __init__(self, name, path, is_dir, stat):
+        self.name = name
+        self.path = path
+        self._is_dir = is_dir
+        self._stat = stat
+
+    def is_dir(self):
+        return self._is_dir
+
+    def stat(self):
+        return self._stat
+
+
+@mock.patch("os.scandir")
+@mock.patch.object(Crawler, "crawl_for_files_in_folders")
+def test_crawl_folder_finds_target_dir(mock_crawl, mock_scandir, ):
+    mock_scandir.side_effect = [[PseudoDirEntry("derp", "/derp/", True, None)],
+                                [PseudoDirEntry("2000-01-01-01", "/2000-01-01-01/", True, None)],
+                                [PseudoDirEntry("derp", "/derp/", False, None)], ]
+    q = Queue()
+    directory = ""
+
+    crawler = Crawler()
+    crawler.crawl_folders(q, directory, from_date=datetime(year=1000, month=1, day=1, tzinfo=timezone.utc),
+                          to_date=datetime(year=5000, month=1, day=1, tzinfo=timezone.utc))
+
+    mock_crawl.assert_called_once()
+
+
+@mock.patch("os.scandir")
+@mock.patch.object(Crawler, "crawl_for_files_in_folders")
+def test_crawl_folder_finds_target_dir_in_daterange(mock_crawl, mock_scandir, ):
+    mock_scandir.return_value = [PseudoDirEntry("1999-01-01-01", "/1999-01-01-01/", True, None),
+                                 PseudoDirEntry("2000-02-01-01", "/2000-02-01-01/", True, None),
+                                 PseudoDirEntry("2001-01-01-01", "/1999-01-01-01/", True, None)]
+    q = Queue()
+    directory = ""
+
+    crawler = Crawler()
+    crawler.crawl_folders(q, directory, from_date=datetime(year=2000, month=1, day=1, tzinfo=timezone.utc),
+                          to_date=datetime(year=2000, month=6, day=1, tzinfo=timezone.utc))
+
+    mock_crawl.assert_called_once()
+
+
+@mock.patch("os.scandir")
+@mock.patch.object(Crawler, "crawl_for_files_in_folders")
+def test_crawl_folder_check_if_not_folder(mock_crawl, mock_scandir, ):
+    mock_scandir.return_value = [PseudoDirEntry("1999-01-01-01", "/1999-01-01-01/", False, None),
+                                 PseudoDirEntry("2000-02-01-01", "/2000-02-01-01/", False, None),
+                                 PseudoDirEntry("2001-01-01-01", "/1999-01-01-01/", False, None)]
+    q = Queue()
+    directory = ""
+
+    crawler = Crawler()
+    crawler.crawl_folders(q, directory, from_date=datetime(year=2000, month=1, day=1, tzinfo=timezone.utc),
+                          to_date=datetime(year=2000, month=6, day=1, tzinfo=timezone.utc))
+
+    mock_crawl.assert_not_called()
