86f04f31ffba49d869ab15f909ae7d0641111bd8
nielswps <nielswps@gmail.com>
2020-12-01 11:03:50 +0100

    segment_group_handler end_group unit tests added

--- alto_segment_lib/tests/test_segment_group_helper.py
+++ alto_segment_lib/tests/test_segment_group_helper.py
@@ -1,44 +1,85 @@
-from alto_segment_lib.segment import SegmentGroup, Segment, SegmentType
-from alto_segment_lib.segment_helper import SegmentHelper
+from alto_segment_lib.segment import Line
 from alto_segment_lib.segment_group_handler import *
 
 
 class TestSegmentGroupHelper:
 
     def test_start_group_sets_unfinished_group_to_current_group(self):
-        segment_handler = SegmentGroupHandler()
+        segment_group_handler = SegmentGroupHandler()
         paragraph = Segment()
         paragraph.type = SegmentType.paragraph
 
-        segment_handler.start_group()
-        segment_handler.add_segment(paragraph)
+        segment_group_handler.start_group()
+        segment_group_handler.add_segment(paragraph)
 
-        segment_handler.start_group()
+        segment_group_handler.start_group()
 
-        assert segment_handler.unfinished_group.paragraphs.__contains__(paragraph)
+        assert segment_group_handler.unfinished_group.paragraphs.__contains__(paragraph)
 
     def test_start_group_adds_unfinished_group_to_groups_when_new_group_is_started(self):
-        segment_handler = SegmentGroupHandler()
+        segment_group_handler = SegmentGroupHandler()
         paragraph = Segment()
         paragraph.type = SegmentType.paragraph
 
-        segment_handler.start_group()
-        segment_handler.add_segment(paragraph)
+        segment_group_handler.start_group()
+        segment_group_handler.add_segment(paragraph)
 
-        segment_handler.start_group()
-        segment_handler.add_segment(paragraph)
+        segment_group_handler.start_group()
+        segment_group_handler.add_segment(paragraph)
 
-        group = segment_handler.unfinished_group
+        group = segment_group_handler.unfinished_group
 
-        segment_handler.start_group()
+        segment_group_handler.start_group()
 
-        assert segment_handler.groups.__contains__(group)
+        assert segment_group_handler.groups.__contains__(group)
 
     def test_start_group_sets_current_group_to_empty_group(self):
-        segment_handler = SegmentGroupHandler()
+        segment_group_handler = SegmentGroupHandler()
 
-        segment_handler.start_group()
+        segment_group_handler.start_group()
 
-        assert segment_handler.current_group.paragraphs.__len__() == 0 and segment_handler.current_group.headers.__len__() == 0
+        assert segment_group_handler.current_group.paragraphs.__len__() == 0 and segment_group_handler.current_group.headers.__len__() == 0
+
+    def test_add_segment(self):
+        assert True
+
+    def test_end_group_sets_current_group_to_none(self):
+        segment_group_handler = SegmentGroupHandler()
+
+        segment_group_handler.start_group()
+        segment_group_handler.end_group()
+
+        assert segment_group_handler.current_group is None
+
+    def test_end_group_does_not_add_empty_group_to_groups_list(self):
+        segment_group_handler = SegmentGroupHandler()
+
+        segment_group_handler.start_group()
+        segment_group_handler.end_group()
+
+        assert segment_group_handler.groups.__len__() == 0
+
+    def test_end_group_adds_current_group_with_paragraph_to_groups_list(self):
+        segment_group_handler = SegmentGroupHandler()
+        paragraph = Segment()
+        paragraph.type = SegmentType.paragraph
+
+        segment_group_handler.start_group()
+        segment_group_handler.add_segment(paragraph)
+        segment_group_handler.end_group()
+
+        assert segment_group_handler.groups.__len__() == 1
+
+    def test_end_group_adds_current_group_with_header_to_groups_list(self):
+        segment_group_handler = SegmentGroupHandler()
+        heading = Segment()
+        heading.type = SegmentType.heading
+        heading.lines.append(Line())
+
+        segment_group_handler.start_group()
+        segment_group_handler.add_segment(heading)
+        segment_group_handler.end_group()
+
+        assert segment_group_handler.groups.__len__() == 1
 
 
