a861760e9f99137b875da146cccbf7ea717a53a4
Alex <Alexik1998@gmail.com>
2020-12-05 19:04:20 +0100

    Lidt oprydning og forlængelse af linjer mellem segmenter

--- alto_segment_lib/segment_lines/segment_lines.py
+++ alto_segment_lib/segment_lines/segment_lines.py
@@ -45,12 +45,13 @@ class SegmentLines:
         for segment in segments:
             # make a line that is parallel with the left side of the segment
             lines.append(Line([segment.x1 - 2, segment.y1, segment.x1 - 2, segment.y2]))
+            # lines.append(Line([segment.x2 + 2, segment.y1, segment.x2 + 2, segment.y2]))      # Lines on both sides of the segment
 
         return lines
 
     def __fix_and_extend_vertical_lines(self, vertical_lines, segments):
         merge_margin = 10
-        merged_lines = []
+        final_lines = []
         content_bound = SegmentHelper().get_content_bounds(segments)
 
         # Finds similar lines
@@ -69,64 +70,17 @@ class SegmentLines:
             all_affected_segments = self.__find_affected_segments(segments, min_y, max_y, average_x)
 
             if len(affected_segments) == 0 or len(line_group) == 1:
-                merged_lines.append(Line([average_x, min_y, average_x, max_y]))
-                delete_later = 1
+                final_lines.append(Line([average_x, min_y, average_x, max_y]))
             else:
-                blah = 1
-                merged_lines2 = self.__merge_all_lines_not_intersecting_segment(line_group, all_affected_segments)
+                wip_merged_lines = self.__merge_all_lines_not_intersecting_segment(line_group, all_affected_segments)
+                extended_to_bounds_lines = self.__extend_lines(wip_merged_lines, all_affected_segments, content_bound)
+                # extended_lines = extended_to_bounds_lines
+                extended_lines = self.__extend_lines_to_segment_borders(extended_to_bounds_lines, all_affected_segments)
 
-                merged_lines2 = self.__extend_lines(merged_lines2, all_affected_segments, content_bound)
+                for line in extended_lines:
+                    final_lines.append(line)
 
-                for line in merged_lines2:
-                    merged_lines.append(line)
-
-
-                                        # line_group = sorted(line_group, key=lambda line: line.y1)
-                                        # affected_segments = sorted(affected_segments, key=lambda segment: segment.y1)
-                                        #
-                                        # # Extends the top line to the page bound
-                                        # if len(self.__find_segments_above_line(line_group[0], affected_segments)) == 0:
-                                        #     merged_lines.append(Line([average_x, content_bound[1], average_x, line_group[0].y2]))
-                                        #     line_group.remove(0)
-                                        # for line in line_group:
-                                        #     # if line.y1 < affected_segments[0].y1:
-                                        #     if len(self.__find_segments_above_line(line, affected_segments)) == 0:
-                                        #         merged_lines.append(Line([average_x, content_bound[1], average_x, line.y2]))
-
-
-                # # Don't merge lines that intersect the segment
-                # i = 0
-                # affected_segments = sorted(affected_segments, key=lambda segment: segment.y1)
-                # all_affected_segments = sorted(all_affected_segments, key=lambda segment: segment.y1)
-                #
-                # for segment in affected_segments:
-                #     if i == 0:
-                #         segments_above_line = self.__find_segments_above_line(line_group[i], all_affected_segments)
-                #         if len(segments_above_line) == 0:
-                #             merged_lines.append(Line([average_x, content_bound[1], average_x, segment.y1]))     # Content_bound kan ikke tilføjes uden et tjek først
-                #         else:
-                #             highest_segment = min(segments_above_line, key=lambda segment: segment.y2)
-                #             merged_lines.append(Line([average_x, highest_segment.y2, average_x, segment.y1]))
-                #     # elif i < len(affected_segments):
-                #     #     previous_segment = affected_segments[i - 1]
-                #     #     if previous_segment.y2 - segment.y1 > 150:              # Tror måske den skulle være mindre end 150 istedet?
-                #     #         merged_lines.append(Line([average_x, previous_segment.y2, average_x, segment.y1]))
-                #     # else:
-                #     #     merged_lines.append(Line([average_x, segment.y2, average_x, content_bound[3]]))
-                #
-                #     i += 1
-
-
-
-
-
-
-                # merged_lines2 = self.__merge_all_lines_not_intersecting_segment(line_group, all_affected_segments)
-                # for line in merged_lines2:
-                #     merged_lines.append(line)
-
-
-        return merged_lines
+        return final_lines
 
     @staticmethod
     def __is_line_in_groups(lines_to_be_merged, line):
@@ -152,7 +106,8 @@ class SegmentLines:
 
         return lines_to_be_merged
 
-    def __get_statistics_for_line_group(self, line_group):
+    @staticmethod
+    def __get_statistics_for_line_group(line_group):
         if len(line_group) == 0:
             return 0, 0, 0
 
@@ -195,7 +150,8 @@ class SegmentLines:
 
         return affected_segments
 
-    def __find_segments_above_line(self, line, segments):
+    @staticmethod
+    def __find_segments_above_line(line, segments):
         segments_above_line = []
 
         for segment in segments:
@@ -294,11 +250,30 @@ class SegmentLines:
 
         return extended_lines
 
+    def __extend_lines_to_segment_borders(self, lines, segments):
+        extended_lines = []
 
+        for line in lines:
+            y1 = 0
+            y2 = 0
 
+            above_segments = self.__find_segments_above_line(line, segments)
+            if len(above_segments) == 0:
+                y1 = line.y1
+            else:
+                above_segment = max(above_segments, key=lambda segment: segment.y2)
+                y1 = above_segment.y2
 
+            below_segments = self.__find_segments_below_line(line, segments)
+            if len(below_segments) == 0:
+                y2 = line.y2
+            else:
+                below_segment = min(below_segments, key=lambda segment: segment.y1)
+                y2 = below_segment.y1
 
+            extended_lines.append(Line([line.x1, y1, line.x2, y2]))
 
+        return extended_lines
 
 
 
