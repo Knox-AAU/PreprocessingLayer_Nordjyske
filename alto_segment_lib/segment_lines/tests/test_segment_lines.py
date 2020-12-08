5e3d3a554ffde7d290263a77177e780778dda518
Jakob Lund <jlund18@student.aau.dk>
2020-12-08 10:56:56 +0100

    added new test

--- alto_segment_lib/segment_lines/tests/test_segment_lines.py
+++ alto_segment_lib/segment_lines/tests/test_segment_lines.py
@@ -12,4 +12,18 @@ def test_find_vertical_lines():
     segment_lines = SegmentLines(paragraphs, headers)
     vertical_line = segment_lines.find_vertical_lines()
 
-    assert vertical_line[0] == Line([10,0,10,30])
+    assert vertical_line[0] == Line([10, 0, 10, 30])
+
+
+def test_find_vertical_lines_not_aligned():
+    headers = []
+    paragraphs = []
+
+    paragraphs.append(Segment([80, 0, 80, 10], SegmentType.paragraph))
+    paragraphs.append(Segment([10, 15, 10, 30], SegmentType.paragraph))
+
+    segment_lines = SegmentLines(paragraphs, headers)
+    vertical_line = segment_lines.find_vertical_lines()
+
+    assert vertical_line[0] == Line([80, 0, 80, 10])
+    assert vertical_line[1] == Line([10, 15, 10, 30])
