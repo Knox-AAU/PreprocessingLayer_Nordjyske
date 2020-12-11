5bccfc35b2e6401af878ff72d1d7d5aec85b4a56
LauJosefsen <ljosef18@student.aau.dk>
2020-12-11 11:24:33 +0100

    Fixed error when launching mr with no arguments.

--- main.py
+++ main.py
@@ -24,13 +24,13 @@ if __name__ == '__main__':
 
     # defines from_date argument
     parser.add_argument('-f', '--from', dest="from_date", type=parse_date,
-                        default=datetime(year=1, month=1, day=1, tzinfo=timezone.utc),
+                        default="1000-01-01",
                         help='defines the end date from the collected data.'
                              ' It should be formatted as YYYY-MM-DD. (Default: no)')
 
     # defines toDate argument
     parser.add_argument('-t', '--to', dest="to_date", type=parse_date,
-                        default=datetime(year=9999, month=1, day=1, tzinfo=timezone.utc),
+                        default="5000-01-01",
                         help='defines the end date from the collected data. '
                              'It should be formatted as YYYY-MM-DD. (Default: no)')
 
