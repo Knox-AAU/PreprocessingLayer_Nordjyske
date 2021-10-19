436a60775239323428c646cbcb83ca694c7b709a
krismp99 <kpede19@student.aau.dk>
2021-10-19 12:17:16 +0200

    Script that will POST all JSON files in a folder and its subfolders

---
+++ post_all_json.py
@@ -0,0 +1,17 @@
+import os
+from knox_source_data_io.io_handler import IOHandler
+import re
+import json
+
+def post_all_json(json_path):
+    for dirpath, subdirs, files in os.walk(json_path):
+        for x in files:
+            if x.endswith(".json"):
+                json_path = os.path.join(dirpath, x)
+                print(json_path)
+                with (open(json_path)) as f:
+                    data = json.load(f)
+                    print(data)
+                    IOHandler.post_json(data)
+
+post_all_json("/home/aau/Desktop/output")
