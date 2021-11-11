70208f3efc64510714408f5259f1c7b9e9bca2a8
krismp99 <kpede19@student.aau.dk>
2021-11-11 10:19:30 +0100

    Test to see if black reformats the code

--- post_all_json.py
+++ post_all_json.py
@@ -10,7 +10,15 @@ def post_all_json(json_path):
             if x.endswith(".json"):
                 json_path = os.path.join(dirpath, x)
                 print(json_path)
-                with (open(json_path)) as f:
+
+
+
+                
+                with    (open(json_path))          as                        f:
+
+
+
+
                     data = json.load(f)
                     print(data)
                     IOHandler.post_json(data)
